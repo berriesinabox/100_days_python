df45f25ad9e498116a22adef8cd2f1bfa8c18ec7
shivangi <shivangishekharjha@gmail.com>
2025-07-10 13:09:43 +0530

    Delete kwargs.py

--- kwargs.py
+++
@@ -1,6 +0,0 @@
-def info_person(**kwargs):
-    for key,value in kwargs.items():
-        print(key,value )
-info_person(name="shivi", roll=123 ,branch="ee")
-print()
-info_person(name="raj", roll=122)
