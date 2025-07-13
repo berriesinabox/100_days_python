6127b77554cfc25367d025aaec1a72abce42e9f6
Shivangi Shekhar <shivangishekharjha@gmail.com>
2025-07-13 14:22:13 +0530

    Add files via upload

---
+++ return.py
@@ -0,0 +1,10 @@
+def add(a,b):
+    if a==0 and b==0:
+        return ""
+    else:
+        return a+b
+var_1=int(input("enter first variable: \n"))
+var_2=int(input("enter the second number: \n"))
+
+result=add(var_1,var_2)
+print(result)
