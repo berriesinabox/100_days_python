c6f92ad54794e6bb7761ce4605d7f33990553da2
Shivangi Shekhar <shivangishekharjha@gmail.com>
2025-07-15 23:55:45 +0530

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,3 +1,4 @@
+#on input div by 3 fizz on div by 5 buzz and on div by both 3 and 5 fizzbuzz
 n=int(input("enter the number till you want to print: "))
 for i in range(1,(n+1)):
     if i%3 ==0 and i%5 ==0:
